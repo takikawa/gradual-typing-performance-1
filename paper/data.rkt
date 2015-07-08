4d28af134a9be4a323ab113c35fce1c49da2b076
ben <types@ccs.neu.edu>
2015-07-08 19:55:39 -0400

    updated readme with new suffixtree data

--- paper/data.rkt
+++ paper/data.rkt
@@ -17,7 +17,6 @@
   [SIEVE-DATA      "./data/sieve-2015-06-28T14:08:55.rktd"]
   [SNAKE-DATA      "./data/snake-2015-06-30T14:55:40.rktd"]
   [SUFFIXTREE-DATA "./data/suffixtree-large-06-30.rktd"]
-  ;[SUFFIXTREE-DATA "./data/suffixtree-06-10.rktd"]
   [SYNTH-DATA      "./data/funkytown-2015-06-30T15:34:39.rktd"]
   [TETRIS-DATA     "./data/tetris-2015-06-30T14:53:37.rktd"]
   [ZORDOZ-DATA     "./data/zordoz-04-09.rktd"]
